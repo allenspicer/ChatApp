484f5701f75798f9dc1fe366f585ae1fd09972d0
Allen Spicer <allenspicer@gmail.com>
2016-06-01 14:20:47 -0400

    Updates

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -11,6 +11,8 @@ import Firebase
 
 class ViewController: UIViewController {
     @IBAction func button(sender: UIButton) {
+        
+        //set a value to firebase
         ref.setValue(textfield.text!)
     }
     @IBOutlet weak var label: UILabel!
@@ -20,17 +22,24 @@ class ViewController: UIViewController {
     
     let ref = Firebase(url: "https://spicerwhisper-59eee.firebaseio.com/")
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
-    ref.setValue("Wahoo!")
-        ref.observeEventType(.Value, withBlock: {snapshot in
-            let message = snapshot.value as! String
-            self.label.text = message
-            
-            
-        })
+        //if firebase changes excute a block of code
+//        ref.observeEventType(.Value, withBlock: {snapshot in
+//            
+//            //take the value of a snapshot and interpret as a string
+//            let message = snapshot.value as! String
+//           // set the label text to be that string
+//            self.label.text = message
+//            
+//        })
+        
+        ref.observeEventType(.Value, withBlock:{snapshot in
+            let countOfChildren = snapshot.childrenCount
+            self.label.text = String(countOfChildren)})
+        
     
     
     }
